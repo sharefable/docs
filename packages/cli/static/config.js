a38afd9ba9be36cd5c365fdbfb1fbb26c41aa67e
Shweta <shweta@sharefable.com>
2023-12-18 16:49:06 +0530

    customize heading style

--- packages/cli/static/config.js
+++ packages/cli/static/config.js
@@ -38,6 +38,48 @@ module.exports = {
       fontSize: 16,
       fontFamily: "sans-serif",
       lineHeight: 1.5,
+      h1: {
+        margin: '0 0 24px 0',
+        padding: 0,
+        fontSize: '38px',
+        fontWeight: 700,
+        lineHeight: '48px'
+      },
+      h2: {
+        margin: '0 0 32px 0',
+        padding: 0,
+        fontSize: '32px',
+        fontWeight: 600,
+        lineHeight: '36px'
+      },
+      h3: {
+        margin: '0 0 32px 0',
+        padding: 0,
+        fontSize: '20px',
+        fontWeight: 600,
+        lineHeight: '26px'
+      },
+      h4: {
+        margin: '0 0 24px 0',
+        padding: 0,
+        fontSize: '16px',
+        fontWeight: 600,
+        lineHeight: '22px'
+      },
+      h5: {
+        margin: '0 0 24px 0',
+        padding: 0,
+        fontSize: '16px',
+        fontWeight: 600,
+        lineHeight: '22px'
+      },
+      h6: {
+        margin: '0 0 24px 0',
+        padding: 0,
+        fontSize: '16px',
+        fontWeight: 600,
+        lineHeight: '22px'
+      }
     },
   }
 }
