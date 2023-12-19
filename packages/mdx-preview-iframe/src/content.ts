65107035b11f3bdc01ad76f2c6696f26a8d2608f
Shweta <shweta@sharefable.com>
2023-12-19 17:31:16 +0530

    add index.css

--- packages/mdx-preview-iframe/src/content.ts
+++ packages/mdx-preview-iframe/src/content.ts
@@ -1,4 +1,3 @@
-
 export const config = `{
   "version": "1.0.0",
   "urlMapping": {
@@ -102,6 +101,7 @@ export const initialCode = `
   import Component from "./file.jsx"
   import Layout from "./layout.jsx"
   import config from './config.json'
+  import './index.css'
   const root = createRoot(document.getElementById("root"))
   root.render(<div><Layout config={config}><FallBackComponent><Component/></FallBackComponent></Layout></div>)
 `
@@ -135,7 +135,6 @@ export default class ErrorBoundary extends Component{
     }
   }
 `
-
 
 export const layoutCode = `
 import React, { useState } from 'https://esm.sh/react'
@@ -326,5 +325,105 @@ export const headerCss = `
   color: var(--primary-color);
   background-color: transparent;
 }
+
+`
+
+export const indexCss = `
+  html {
+    font-size: var(--font-size);
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: var(--font-family);
+    line-height: var(--line-height);
+    background-color: var(--background-primary-color);
+    color: var(--text-primary-color);
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--text-primary-color);
+    line-height: var(--line-height);
+  }
+
+  a:hover {
+    background-color: var(--accent-color);
+    color: var(--primary-color);
+  }
+
+  a:active {
+    color: var(--primary-color);
+  }
+
+  h1 {
+    margin: var(--h1-margin);
+    padding: var(--h1-padding);
+    font-size: var(--h1-font-size);
+    line-height: var(--h1-line-height);
+    font-weight: var(--h1-font-weight);
+  }
+
+  h2 {
+    margin: var(--h2-margin);
+    padding: var(--h2-padding);
+    font-size: var(--h2-font-size);
+    line-height: var(--h2-line-height);
+    font-weight: var(--h2-font-weight);
+  }
+
+  h3 {
+    margin: var(--h3-margin);
+    padding: var(--h3-padding);
+    font-size: var(--h3-font-size);
+    line-height: var(--h3-line-height);
+    font-weight: var(--h3-font-weight);
+  }
+
+  h4 {
+    margin: var(--h4-margin);
+    padding: var(--h4-padding);
+    font-size: var(--h4-font-size);
+    line-height: var(--h4-line-height);
+    font-weight: var(--h4-font-weight);
+  }
+
+  h5 {
+    margin: var(--h5-margin);
+    padding: var(--h5-padding);
+    font-size: var(--h5-font-size);
+    line-height: var(--h5-line-height);
+    font-weight: var(--h5-font-weight);
+  }
+
+  h6 {
+    margin: var(--h6-margin);
+    padding: var(--h6-padding);
+    font-size: var(--h6-font-size);
+    line-height: var(--h6-line-height);
+    font-weight: var(--h6-font-weight);
+  }
+
+  .con {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  .main-wrapper {
+    display: flex;
+    flex-grow: 1;
+    align-items: stretch;
+    margin-top: 65px;
+    height: calc(100vh - 65px);
+  }
+
+  .main-con {
+    flex: 3;
+    width: 100%;
+    overflow-y: auto;
+  }
 
 `
