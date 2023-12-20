cb4f0b067165866a86ed2267aa2b74302086827b
siddhigate12 <siddhi@sharefable.com>
2023-12-20 11:35:39 +0530

    feat: get manifest config api

--- packages/github-bot/src/index.ts
+++ packages/github-bot/src/index.ts
@@ -1,10 +1,17 @@
-import { Probot } from "probot";
+import { ApplicationFunctionOptions, Probot } from "probot";
 import { generatePRPreview } from "./build-pr";
+import { getManifestConfig } from "./get-manifest-config";
 
-export default (app: Probot) => {
+export default (app: Probot, { getRouter }: ApplicationFunctionOptions) => {
 
+  // @ts-ignore
   app.on('pull_request.synchronize', generatePRPreview)
 
+  // @ts-ignore
   app.on('pull_request.opened', generatePRPreview);
 
+  if (getRouter) {
+    const router = getRouter()
+    router.get("/hello-world", getManifestConfig);
+  }
 };
