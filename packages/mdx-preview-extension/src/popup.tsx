69e3b482b33dbc7480ce4c5e0230350ea7ba928c
Shweta <shweta@sharefable.com>
2023-12-20 10:23:18 +0530

    refactor code

--- packages/mdx-preview-extension/src/popup.tsx
+++ packages/mdx-preview-extension/src/popup.tsx
@@ -1,6 +1,5 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Popup/App";
+import App from "./popup/App";
 
 ReactDOM.render( <App />, document.getElementById("root"));
