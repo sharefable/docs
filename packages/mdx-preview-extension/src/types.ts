3d23b89d4b2d31de442393f9718be5664650cfe1
shweta <135203546+shweta-fable@users.noreply.github.com>
2023-12-20 19:33:47 +0530

    Post config message from extension to iframe (#13)
    * send config from extension
    
    * refactor. use file name
    
    * get owner name

--- packages/mdx-preview-extension/src/types.ts
+++ packages/mdx-preview-extension/src/types.ts
@@ -1,5 +1,6 @@
 export enum Msg {
     EXTENSION_ACTIVATED = 'extension_activated',
     INVALID_PAGE = 'invalid_page',
-    MDX_DATA = 'mdx_data'
+    MDX_DATA = 'mdx_data',
+    CONFIG_DATA = 'config_data'
 }
--- packages/mdx-preview-iframe/src/content.ts
+++ packages/mdx-preview-iframe/src/content.ts
@@ -1,110 +1,17 @@
-export const config = `{
-  "version": "1.0.0",
-  "urlMapping": {
-    "globalPrefix": "",
-    "entries": {
-      "/": {
-        "filePath": "index",
-        "fileName": "index"
-      }
-    }
-  },
-  "props": {
-    "header": {
-      "logo": {
-        "imageUrl": "https://sharefable.com/fable-logo.svg",
-        "title": "Fable Docs"
-      },
-      "navLinks": {
-        "alignment": "center",
-        "links": [
-          {
-            "title": "Visit Fable",
-            "url": "https://sharefable.com"
-          }
-        ]
-      }
-    },
-    "sidepanel": {
-      "showSidePanel": true
-    },
-    "content": {},
-    "footer": {}
-  },
-  "theme": {
-    "colors": {
-      "primary": "#3730a3",
-      "textPrimary": "#1e293b",
-      "textSecondary": "#ffffff",
-      "textTertiary": "#ffffff",
-      "backgroundPrimary": "#f3f4f6",
-      "backgroundSecondary": "#f3f4f6",
-      "accent": "#c7d2fe",
-      "border": "#d1d5db",
-      "text": "#1e293b",
-      "background": "#f3f4f6"
-    },
-    "typography": {
-      "fontSize": 16,
-      "fontFamily": "sans-serif",
-      "lineHeight": 1.5,
-      "h1": {
-        "margin": "0 0 24px 0",
-        "padding": 0,
-        "fontSize": "38px",
-        "fontWeight": 700,
-        "lineHeight": "48px"
-      },
-      "h2": {
-        "margin": "0 0 32px 0",
-        "padding": 0,
-        "fontSize": "32px",
-        "fontWeight": 600,
-        "lineHeight": "36px"
-      },
-      "h3": {
-        "margin": "0 0 32px 0",
-        "padding": 0,
-        "fontSize": "20px",
-        "fontWeight": 600,
-        "lineHeight": "26px"
-      },
-      "h4": {
-        "margin": "0 0 24px 0",
-        "padding": 0,
-        "fontSize": "16px",
-        "fontWeight": 600,
-        "lineHeight": "22px"
-      },
-      "h5": {
-        "margin": "0 0 24px 0",
-        "padding": 0,
-        "fontSize": "16px",
-        "fontWeight": 600,
-        "lineHeight": "22px"
-      },
-      "h6": {
-        "margin": "0 0 24px 0",
-        "padding": 0,
-        "fontSize": "16px",
-        "fontWeight": 600,
-        "lineHeight": "22px"
-      }
-    }
-  }
-}`
+import { FileName } from "./types"
 
 export const initialCode = `
   import React from "https://esm.sh/react@18.2.0"
   import { createRoot } from "https://esm.sh/react-dom@18.2.0/client"
   import { BrowserRouter } from 'https://esm.sh/react-router-dom'
   import FallBackComponent from './fallBack.jsx'
-  import Component from "./file.jsx"
+  import Component from "./${FileName.MDX_BUILD_JSX}"
   import Layout from "./layout.jsx"
-  import config from './config.json'
+  import config from './${FileName.CONFIG_JSON}'
+  import manifest from './${FileName.MANIFEST_JSON}'
   import './index.css'
   const root = createRoot(document.getElementById("root"))
-  root.render( <BrowserRouter><div><Layout config={config}><FallBackComponent><Component/></FallBackComponent></Layout></div> </BrowserRouter>)
+  root.render( <BrowserRouter><div><Layout config={config}><FallBackComponent><Component manifest={manifest}/></FallBackComponent></Layout></div> </BrowserRouter>)
 `
 
 export const fallbackCode = `
