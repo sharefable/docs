ac085c8c0262735901d1fd602b85f52d5b9d72a4
Kedar Basutkar <kedar-int@sharefable.com>
2023-12-22 17:46:39 +0530

    fix: meta tags duplication

--- packages/cli/static/Wrapper.js
+++ packages/cli/static/Wrapper.js
@@ -1,76 +1,98 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useRef } from "react";
 
 export default function Wrapper(props) {
-  console.log(props.frontmatter)
-  console.log(typeof props.frontmatter)
+  const flagRef = useRef(false)
+
   useEffect(() => {
+    if (flagRef.current) return
+    
     function generateHeadTag(obj) {
       const head = document.head;
       const tags = {
-        meta: [
+        meta: [          
           {
             name: 'description',
-            content: obj.description,
-          },
-          {
-            property: 'og:title',
-            content: obj.title,
-          },
-          {
-            property: 'og:image',
-            content: obj?.url,
+            content: obj?.description,
+            id: 'description',
           },
           {
             property: 'og:description',
-            content: obj.description
+            content: obj?.ogDescription,
+            id: 'og-description',
+          },
+          {
+            property: 'og:title',
+            content: obj?.ogTitle,
+            id: 'og-title',
+          },
+          {
+            property: 'og:image',
+            content: obj?.ogImage,
+            id: 'og-image',
           },
           {
             property: 'og:type',
-            content: 'website'
+            content: 'website',
+            id: 'og-type',
           },
           {
             property: 'og:url',
-            content: obj?.url
+            content: obj?.ogUrl,
+            id: 'og-url',
           },
           {
             name: 'twitter:card',
-            content: 'summary_large_image'
+            content: 'summary_large_image',
+            id: 'twitter-card',
           },
           {
             property: 'twitter:domain',
-            content: obj?.url
+            content: obj?.twitterDomain,
+            id: 'twitter-domain',
           },
           {
             property: 'twitter:url',
-            content: obj?.url
+            content: obj?.twitterUrl,
+            id: 'twitter-url',
           },
           {
             name: 'twitter:title',
-            content: obj.title
+            content: obj?.twitterTitle,
+            id: 'twitter-title',
           },
           {
             name: 'twitter:description',
-            content: obj.description
+            content: obj?.twitterDescription,
+            id: 'twitter-description',
           },
           {
             name: 'twitter:image',
-            content: obj?.url
+            content: obj?.twitterImage,
+            id: 'twitter-image',
           },
         ],
       }
-      const generatedTags = tags.meta.filter(tag => typeof tag.content === 'string').map((tag) => {
+
+      const generatedTags = tags.meta
+      .filter(tag => typeof tag.content === 'string')
+      .filter(tag => !document.getElementById(tag.id))
+      .map((tag) => {
         const meta = document.createElement('meta');
         Object.entries(tag).forEach(([key, value]) => {
           meta.setAttribute(key, value);
         })
         return meta;
       })
-      console.log(generatedTags)
+
       head.append(...generatedTags);
+
+      document.title = obj.title
+
+      flagRef.current = true
     };
-    generateHeadTag(props)
-  }, [props])
+
+    generateHeadTag(props.frontmatter)
+  }, [props.frontmatter])
 
   return (
     <>
