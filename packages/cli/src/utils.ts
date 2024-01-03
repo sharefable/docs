9fc578bd1a5ac2fff2eab16806f3ffc212666d30
siddhigate12 <siddhi@sharefable.com>
2024-01-03 10:59:54 +0530

    fix: use wrapper component

--- packages/cli/src/utils.ts
+++ packages/cli/src/utils.ts
@@ -60,7 +60,9 @@ const getRouterConfig = (urlMap: UrlEntriesMap, globalPrefix: string): string[] 
                 {...props}/>
               }
             >
-              <${convertToPascalCase(entry.filePath)} globalState={globalState} addToGlobalState={addToGlobalState} manifest={manifest} config={config} />
+              <Wrapper frontmatter={${JSON.stringify(entry.frontmatter)}}>
+                <${convertToPascalCase(entry.filePath)} globalState={globalState} addToGlobalState={addToGlobalState} manifest={manifest} config={config} />
+              </Wrapper>
           </Layout>
         }
       />
