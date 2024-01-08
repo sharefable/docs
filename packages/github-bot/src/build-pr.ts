67085edf2b83bf351e23e759c3feb6f3f770a211
akash <akash@sharefable.com>
2024-01-08 12:43:42 +0000

    Change back debug statements

--- packages/github-bot/src/build-pr.ts
+++ packages/github-bot/src/build-pr.ts
@@ -41,7 +41,6 @@ export async function generatePRPreview(context: Context<"pull_request">) {
     const token = await context.octokit.apps.createInstallationAccessToken({
       installation_id: installationId
     });
-    context.log.info("token " + token.data.token);
     context.log.info("Cloning the repo...");
     execSync(`git clone --depth 1 https://x-access-token:${token.data.token}@github.com/${owner}/${repo}.git ${repoDir}`);
     isTempDirCreated = true;
@@ -116,7 +115,7 @@ export async function generatePRPreview(context: Context<"pull_request">) {
     // await context.octokit.apps.revokeInstallationAccessToken()
     if (isTempDirCreated) {
       context.log.info("Removing temporary file");
-      // await rm(repoDir, { recursive: true })
+      await rm(repoDir, { recursive: true })
     }
   }
 }
