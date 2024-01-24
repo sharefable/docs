88761006fdb1cf14faae40b74dbbe781284ff145
Shweta <shweta@sharefable.com>
2024-01-24 12:49:03 +0530

    fix listener adding multiple times

--- packages/mdx-preview-extension/src/editor-content.ts
+++ packages/mdx-preview-extension/src/editor-content.ts
@@ -1,9 +1,24 @@
 import { EditorContent, Msg } from "./types";
 import { GITHUB_EDIT_TAB_SELECTOR } from "./utils";
 
-window.addEventListener("message", (event) => {
-  if (event.data.type === Msg.GET_EDITOR_DATA) {
-    const editorElement = document.querySelector(GITHUB_EDIT_TAB_SELECTOR) as any as EditorContent;
-    window.postMessage({ type: Msg.EDITOR_DATA, data: editorElement.cmView.view.state.doc.toString() });
-  }
-});
+const DOCDEN_CONTENT_SCRIPT_DIV_ID = "docden-1-cm-presence";
+
+function handleEditorContent(){
+  window.addEventListener("message", (event) => {
+    if (event.data.type === Msg.GET_EDITOR_DATA) {
+      const editorElement = document.querySelector(GITHUB_EDIT_TAB_SELECTOR) as any as EditorContent;
+      window.postMessage({ type: Msg.EDITOR_DATA, data: editorElement.cmView.view.state.doc.toString() });
+    }
+  });
+}
+
+function createDocDenZeroPx() {
+  const div = document.createElement("div");
+  div.setAttribute("id", DOCDEN_CONTENT_SCRIPT_DIV_ID);
+  document.body.appendChild(div);
+}
+
+if (document.getElementById(DOCDEN_CONTENT_SCRIPT_DIV_ID) == null) {
+  createDocDenZeroPx();
+  handleEditorContent();
+}
