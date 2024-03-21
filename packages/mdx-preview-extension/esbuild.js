b538ab3b6fcf8760a56a763363c79992e67ac4fc
siddhigate12 <siddhi@sharefable.com>
2024-03-21 18:09:40 +0530

    chore: add scripts

--- packages/mdx-preview-extension/esbuild.js
+++ packages/mdx-preview-extension/esbuild.js
@@ -1,26 +1,35 @@
 import { build } from "esbuild";
 import { copy } from "esbuild-plugin-copy";
+import {
+  readFileSync,
+} from "fs";
+
+const configDev = JSON.parse(readFileSync("./configs/config.dev.json", "utf8"));
+const configProd = JSON.parse(readFileSync("./configs/config.prod.json", "utf8"));
 
 build({
-    entryPoints: {
-        popup: "./src/popup.tsx",
-        content: "./src/content.ts",
-        editorContent: "./src/editor-content.ts",
-        background: "./src/background.ts"
-    },
-    bundle: true,
-    outdir: "./build",
-    minify: true,
-    sourcemap: "inline",
-    plugins: [
-        copy({
-            resolveFrom: "cwd",
-            assets: [
-                {
-                    from: ["./public/*"],
-                    to: ["./build/"],
-                },
-            ],
-        }),
-    ],
+  entryPoints: {
+    popup: "./src/popup.tsx",
+    content: "./src/content.ts",
+    editorContent: "./src/editor-content.ts",
+    background: "./src/background.ts"
+  },
+  bundle: true,
+  outdir: "./build",
+  minify: true,
+  sourcemap: "inline",
+  define: {
+    "process.env.CONFIG": JSON.stringify(process.env.NODE_ENV === "production" ? configProd : configDev)
+  },
+  plugins: [
+    copy({
+      resolveFrom: "cwd",
+      assets: [
+        {
+          from: ["./public/*"],
+          to: ["./build/"],
+        },
+      ],
+    }),
+  ],
 });
